780c6be86955309485e76513c9079800790ac4bb
Yakiv Mospan <yakiv.mospan@gmail.com>
2026-02-10 10:47:24 +0100

    Unused code clean up.

--- shared/src/commonMain/kotlin/com/yakivmospan/templates/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.yakivmospan.templates
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/yakivmospan/templates/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.yakivmospan.templates
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
