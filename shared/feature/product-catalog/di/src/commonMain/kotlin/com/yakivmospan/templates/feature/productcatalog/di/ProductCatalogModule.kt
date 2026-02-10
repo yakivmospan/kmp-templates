0138e9f7924ac6654383ed928d1b71c759a5ef11
Yakiv Mospan <yakiv.mospan@gmail.com>
2026-02-10 12:12:22 +0100

    Added product catalog di module.

---
+++ shared/feature/product-catalog/di/src/commonMain/kotlin/com/yakivmospan/templates/feature/productcatalog/di/ProductCatalogModule.kt
@@ -0,0 +1,37 @@
+package com.yakivmospan.templates.feature.productcatalog.di
+
+import com.yakivmospan.templates.feature.productcatalog.data.mapper.PaginatedProductsMapper
+import com.yakivmospan.templates.feature.productcatalog.data.mapper.ProductMapper
+import com.yakivmospan.templates.feature.productcatalog.data.remote.ProductRemoteDataSource
+import com.yakivmospan.templates.feature.productcatalog.data.repository.ProductRepositoryImpl
+import com.yakivmospan.templates.feature.productcatalog.domain.repository.ProductRepository
+import com.yakivmospan.templates.feature.productcatalog.domain.usecase.GetProductByIdUseCase
+import com.yakivmospan.templates.feature.productcatalog.domain.usecase.GetProductsUseCase
+import com.yakivmospan.templates.feature.productcatalog.domain.usecase.SearchProductsUseCase
+import com.yakivmospan.templates.feature.productcatalog.presentation.ProductCatalogViewModel
+import org.koin.dsl.module
+
+val productCatalogModule = module {
+    // Data layer
+    single { ProductMapper() }
+    single { PaginatedProductsMapper(productMapper = get()) }
+    single { ProductRemoteDataSource(httpClient = get()) }
+
+    single<ProductRepository> {
+        ProductRepositoryImpl(
+            remoteDataSource = get(),
+            productMapper = get(),
+            paginatedProductsMapper = get(),
+            exceptionMapper = get(),
+            dispatchers = get()
+        )
+    }
+
+    // Domain layer
+    factory { GetProductsUseCase(get()) }
+    factory { GetProductByIdUseCase(get()) }
+    factory { SearchProductsUseCase(get()) }
+
+    // Presentation layer
+    factory { ProductCatalogViewModel(get(), get()) }
+}
