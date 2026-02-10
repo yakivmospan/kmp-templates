a079fa72976a402ae8a0ce4e9f3c9c4efc0e3785
Yakiv Mospan <yakiv.mospan@gmail.com>
2026-02-10 12:12:35 +0100

    Added product catalog feature.

---
+++ shared/feature/product-catalog/src/commonMain/kotlin/com/yakivmospan/templates/feature/productcatalog/ProductCatalogFeature.kt
@@ -0,0 +1,8 @@
+package com.yakivmospan.templates.feature.productcatalog
+
+// This is a facade/umbrella module - it re-exports all layers
+// Apps just depend on this module to get everything
+
+object ProductCatalogFeature {
+    const val FEATURE_NAME = "ProductCatalog"
+}
